b40070014a334ba81fdf541450db7b9e5212a51a
LJEsp <esparragoleandroj@gmail.com>
2019-01-26 19:40:31 +0800

    Fix

--- pages/001_sentiment/pages/index.js
+++
@@ -1,5 +0,0 @@
-import ComponentsPage from "./ComponentsPage/ComponentsPage";
-import Login from "./Login/Login";
-import Main from "./Main/Main";
-
-export { ComponentsPage, Login, Main };
--- pages/001_sentiment/templates/index.js
+++
@@ -1,4 +0,0 @@
-import SelfPost from "./SelfPost/SelfPost";
-import NormalPost from "./NormalPost/NormalPost";
-
-export { SelfPost, NormalPost };
